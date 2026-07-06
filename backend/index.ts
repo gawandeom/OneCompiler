3b588b305d423d6df8017033249695cb550bc6b8
gawandeom <gawandeom11@gmail.com>
2026-07-06 17:52:02 +0530

    feat: integrate Prisma with PostgreSQL and set up initial user model
    - Added db.ts to configure Prisma client with PostgreSQL adapter.
    - Updated index.ts to include health check endpoint that fetches users from the database.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration for User table with email and name fields.
    - Created schema.prisma to define the User model.
    - Updated package.json to include necessary dependencies for Prisma and PostgreSQL.

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,22 @@
-import express from "express"
-
+import express, { urlencoded } from "express"
+import "dotenv/config";
+import { prisma } from "./db";
+import cookieParser from "cookie-parser"
 const app = express()
 
-app.get('/health',(req,res)=>{
-    res.send("Health check ")
+app.use(express.json({
+    limit:"16kb"
+}))
+app.use(urlencoded({
+    extended:true,
+}))
+
+app.use(cookieParser());
+
+app.get('/health',async(req,res)=>{
+const users = await prisma.user.findMany()
+console.log(users)
+    res.send(`user: ${users}`)
 }
 )
 
