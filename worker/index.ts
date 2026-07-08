77eecb86249c68ecfaa085454f9252c51f62351c
gawandeom <gawandeom11@gmail.com>
2026-07-08 17:03:27 +0530

    feat: add initial worker setup with Prisma and Redis integration
    - Created a new JavaScript file for logging changes.
    - Added TypeScript file for Prisma client setup with PostgreSQL adapter.
    - Implemented worker logic to execute JavaScript code from Redis queue and handle results.
    - Updated package.json to include necessary dependencies for Prisma and PostgreSQL.
    - Added Prisma configuration file and schema definition for the database.
    - Created initial database migration scripts for User and Sumission tables.
    - Made error field in Sumission table optional in a subsequent migration.
    - Added migration lock file for version control.

--- worker/index.ts
+++ worker/index.ts
@@ -1,5 +1,9 @@
 import {  createClient } from "redis";
-
+import {spawn} from "node:child_process"
+import fs from "fs"
+import { prisma } from "./db.ts";
+import { resolve } from "node:dns";
+import { compileFunction } from "node:vm";
 
 const client = createClient()
 
@@ -8,22 +12,116 @@ client.on('error',(err)=>{console.log( "Redis connection error"
 
 
 
-async function startWorker(){
+async function executor() {
+  while(true){
 
-    try {
-        await client.connect()
-        console.log("Worker Connected to Redis")
 
-        while(true){
             const response = await client.rPop('problems')
+
             if (!response) {
                 await new Promise((r) => setTimeout(r, 1000));
                 continue;
             }
             
             let parsedResponse= JSON.parse(response)
-            console.log(parsedResponse.code) 
-        }
+            let {id,code,lang} = parsedResponse
+            let finalOutput = ""
+            let finalError = ""
+
+
+
+
+            
+            if(lang==='js'){
+
+                const filepath = __dirname + "/code/a.js"
+                fs.writeFileSync(filepath,code)
+
+                const CompilerResponse = spawn("node",[filepath])
+
+                CompilerResponse.stdout.on("data",(chunk)=>{
+                    finalOutput += chunk.toString();
+                    
+                    console.log(finalOutput)
+                })
+
+                 CompilerResponse.stderr.on("error",(error)=>{
+                        finalError += error.toString();
+                })
+
+              await  new Promise<void>((resolve) =>{
+                    
+                CompilerResponse.on("exit",async(exitCode)=>{
+                    if(exitCode === 0){
+                        const res =   await prisma.sumission.update({
+                        
+                            where:{
+                                id
+                            },
+                            data:{
+                                status:"sucess",
+                                error:"null",
+                                output:finalOutput
+                            }
+                        })
+                        console.log(res)
+
+                    }else{
+
+                        await prisma.sumission.update({
+                            where:{
+                                id
+                            },
+                            data:{
+                                status:"failure",
+                                error:finalError,
+                                output:finalOutput
+                            }
+                        })
+
+                    }
+                })
+                    
+                    resolve()
+                })
+            
+
+               
+
+            }
+        }  
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function startWorker(){
+
+    try {
+        await client.connect().then()
+        console.log("Worker Connected to Redis")
+
+        
+        executor()
 
     } catch (error) {
         console.log(`error in coonecting to redis ${error}`)
